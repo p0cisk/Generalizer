cf4c775bb072c8ffb5592d41810b468cfdcec83d
Sylvain POULAIN <kikislater@users.noreply.github.com>
2020-11-09 17:15:47 +0400

    Move plugin to his self toolbar
        0.9 (09.11.2020)
            - Move plugin to his self toolbar

--- generalizer.py
+++ generalizer.py
@@ -34,28 +34,165 @@ from qgis.core import *
 from .resources import *
 # Import the code for the dialog
 from .generalizerdialog import generalizerDialog, getLayersNames
+import os.path
 
-class generalizer(object):
+
+class generalizer:
 
     def __init__(self, iface):
         # Save reference to the QGIS interface
         self.iface = iface
+        
+        self.canvas = iface.mapCanvas()
+
+        # initialize plugin directory
+        self.plugin_dir = os.path.dirname(__file__)
+
+        # Declare instance attributes
+        self.actions = []
+        self.menu = self.tr(u'&Generalizer')
+        # TODO: We are going to let the user set this up in a future iteration
+        self.toolbar = self.iface.addToolBar(u'Generalizer')
+        self.toolbar.setObjectName(u'Generalizer')
+
+        #print "** INITIALIZING Generalizer"
+
+        self.pluginIsActive = False
+        self.dockwidget = None
+
+        self.output_layer = None
+
+    # noinspection PyMethodMayBeStatic
+    def tr(self, message):
+        """Get the translation for a string using Qt translation API.
+
+        We implement this ourselves since we do not inherit QObject.
+
+        :param message: String for translation.
+        :type message: str, QString
+
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
+        return QCoreApplication.translate('Generalizer', message)
+        
+    def add_action(
+        self,
+        icon_path,
+        text,
+        callback,
+        enabled_flag=True,
+        add_to_menu=True,
+        add_to_toolbar=True,
+        status_tip=None,
+        whats_this=None,
+        parent=None):
+        """Add a toolbar icon to the toolbar.
+
+        :param icon_path: Path to the icon for this action. Can be a resource
+            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
+        :type icon_path: str
+
+        :param text: Text that should be shown in menu items for this action.
+        :type text: str
+
+        :param callback: Function to be called when the action is triggered.
+        :type callback: function
+
+        :param enabled_flag: A flag indicating if the action should be enabled
+            by default. Defaults to True.
+        :type enabled_flag: bool
+
+        :param add_to_menu: Flag indicating whether the action should also
+            be added to the menu. Defaults to True.
+        :type add_to_menu: bool
+
+        :param add_to_toolbar: Flag indicating whether the action should also
+            be added to the toolbar. Defaults to True.
+        :type add_to_toolbar: bool
+
+        :param status_tip: Optional text to show in a popup when mouse pointer
+            hovers over the action.
+        :type status_tip: str
+
+        :param parent: Parent widget for the new action. Defaults None.
+        :type parent: QWidget
+
+        :param whats_this: Optional text to show in the status bar when the
+            mouse pointer hovers over the action.
+
+        :returns: The action that was created. Note that the action is also
+            added to self.actions list.
+        :rtype: QAction
+        """
+
+        icon = QIcon(icon_path)
+        text = "Generalizer"
+        action = QAction(icon, text, parent)
+        action.triggered.connect(callback)
+        action.setEnabled(enabled_flag)
+
+        if status_tip is not None:
+            action.setStatusTip(status_tip)
+
+        if whats_this is not None:
+            action.setWhatsThis(whats_this)
+
+        if add_to_toolbar:
+            self.toolbar.addAction(action)
+
+        if add_to_menu:
+            self.iface.addPluginToVectorMenu(
+                self.menu,
+                action)
+
+        self.actions.append(action)
+
+        return action
+
 
     def initGui(self):
-        # Create action that will start plugin configuration
-        self.action = QAction(QIcon(":/plugins/generalizer/icon.png"), \
-            "Generalizer", self.iface.mainWindow())
-        # connect the action to the run method
-        self.action.triggered.connect( self.run )
+        """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        # Add toolbar button and menu item
-        self.iface.addToolBarIcon(self.action)
-        self.iface.addPluginToMenu("&Generalizer", self.action)
+        icon_path = ':/plugins/Generalizer3/icon.png'
+        self.add_action(
+            icon_path,
+            text=self.tr(u''),
+            callback=self.run,
+            parent=self.iface.mainWindow())
+
+    #--------------------------------------------------------------------------
+
+    def onClosePlugin(self):
+        """Cleanup necessary items here when plugin dockwidget is closed"""
+
+        #print "** CLOSING Generalizer"
+
+        # disconnects
+        self.dockwidget.closingPlugin.disconnect(self.onClosePlugin)
+
+        # remove this statement if dockwidget is to remain
+        # for reuse if plugin is reopened
+        # Commented next statement since it causes QGIS crashe
+        # when closing the docked window:
+        # self.dockwidget = None
+
+        self.pluginIsActive = False
+
 
     def unload(self):
-        # Remove the plugin menu item and icon
-        self.iface.removePluginMenu("&Generalizer",self.action)
-        self.iface.removeToolBarIcon(self.action)
+        """Removes the plugin menu item and icon from QGIS GUI."""
+
+        #print "** UNLOAD Generalizer"
+
+        for action in self.actions:
+            self.iface.removePluginVectorMenu(
+                self.tr(u'&Generalizer'),
+                action)
+            self.iface.removeToolBarIcon(action)
+        # remove the toolbar
+        del self.toolbar
 
     # run method that performs all the real work
     def run(self):
